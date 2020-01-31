67132aea0719d954ae7ce7051b27d804acdd1295
Rui <ruipcc1@gmail.com>
2020-01-31 15:06:56 +0000

    All da codez

---
+++ New Unity Project (1)/Assets/Script/ImageTargetPlayAudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetPlayAudio : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    public bool playing;
+    public string number;
+    public string Input;
+    public string tempInput;
+    
+    void Start()
+    {
+        playing = false;
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            GetComponent<AudioSource>().Play();
+            playing = true;
+            tempInput = number;
+            Input = Input + tempInput;
+        }
+        else
+        {
+            // Stop audio when target is lost
+            GetComponent<AudioSource>().Stop();
+            playing = false;
+        }
+    }
+}
---
+++ New Unity Project (1)/Assets/Script/PlaySound.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioClip sound;
+    private AudioSource audioSource;
+    public ImageTargetPlayAudio script;
+    public string code;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(script.playing == true)
+        {
+            GetComponent<AudioSource>().Stop();
+            StartCoroutine(Wait());
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
+        if (code == (script.Input))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        else if (code != (script.Input))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if(script.playing == false)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
