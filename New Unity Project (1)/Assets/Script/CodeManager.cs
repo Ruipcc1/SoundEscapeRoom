41a16ac07bd1d6f6979e89a4fbcc0f490ea54fe6
Unknown <ruipcc1@gmail.com>
2020-02-19 11:19:45 +0000

    FINAL

--- New Unity Project (1)/Assets/Script/CodeManager.cs
+++ New Unity Project (1)/Assets/Script/CodeManager.cs
@@ -8,20 +8,31 @@ public class CodeManager : MonoBehaviour
     public bool playing;
     public bool winning;
     public string Input;
+    bool won;
     // Start is called before the first frame update
     void Start()
     {
-        code = "123";
+        winning = false;
+        code = "12";
         Input = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input == code)
-        {
-            winning = true;
-            GetComponent<AudioSource>().Play();
+        if(winning == false) {
+            if (Input == code)
+            {
+                winning = true;
+                GetComponent<AudioSource>().Play();
+                StartCoroutine(Wait());
+            }
         }
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
+        GetComponent<AudioSource>().Play();
+        StartCoroutine(Wait());
     }
 }
--- New Unity Project (1)/Assets/Script/PlaySound.cs
+++ New Unity Project (1)/Assets/Script/PlaySound.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class PlaySound : MonoBehaviour
 {
     public CodeManager script;
+    bool repeat;
     // Start is called before the first frame update
     void Start()
     {
+        repeat = false;
         GetComponent<AudioSource>().Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(script.playing == true)
-        {
-            GetComponent<AudioSource>().Stop();
-            StartCoroutine(Wait());
+        if (repeat == false) {
+            if (script.playing == true)
+            {
+                GetComponent<AudioSource>().Stop();
+                StartCoroutine(Wait());
+            }
         }
     }
 
     IEnumerator Wait()
     {
+        repeat = true;
         yield return new WaitForSeconds(10);
         if(script.winning == false)
         {
@@ -33,6 +38,7 @@ public class PlaySound : MonoBehaviour
         {
             StartCoroutine(Win());
         }
+        repeat = false;
     }
 
     IEnumerator Win()
@@ -40,6 +46,6 @@ public class PlaySound : MonoBehaviour
         yield return new WaitForSeconds(60);
             script.Input = "";
         GetComponent<AudioSource>().Play();
-
+        script.winning = false;
     }
 }
